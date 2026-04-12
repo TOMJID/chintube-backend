3544bfa3b59bb6314b9a669d155645ec7461af1c
TOMJID <tomjidhuda04@gmail.com>
2026-04-12 18:47:51 +0600

    feat(auth): add authentication system
    - Add better-auth dependency
    - Define Prisma models for auth
    - Generate Prisma migration
    - Integrate auth routes into app
    - Configure better-auth with adapter

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
+import { toNodeHandler } from "better-auth/node";
 import express, { Application, Request, Response } from "express";
+import { auth } from "./app/lib/auth";
 
 
 const app: Application = express();
+
+//! better auth
+app.all("/api/auth/*any", toNodeHandler(auth));
 
 app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Hello, World!" });
---
+++ src/app/lib/auth.ts
@@ -0,0 +1,26 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "./prisma";
+import { Role } from "../../../generated/prisma/enums";
+
+export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
+  secret: process.env.BETTER_AUTH_SECRET,
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+
+  emailAndPassword: {
+    enabled: true,
+  },
+
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        defaultValue: Role.USER,
+      }
+    }
+  }
+
+});
